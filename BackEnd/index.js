13ea37a7153d5ebb9640284da853ebc8ade2e337
nabilyahya <nabeelhero12@gmail.com>
2023-04-01 18:44:09 +0300

    test

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,9 +1,6 @@
 const express = require("express");
-
 const cors = require("cors");
-
 const bodyParser = require("body-parser");
-
 const { Configuration, OpenAIApi } = require("openai");
 
 const config = new Configuration({
@@ -12,24 +9,27 @@ const config = new Configuration({
 
 const openai = new OpenAIApi(config);
 
-// Setup server
-
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-// endpoint for ChatGPT
-
 app.post("/chat", async (req, res) => {
-  const { prompt } = req.body;
+  try {
+    const { prompt } = req.body;
 
-  const completion = await openai.createCompletion({
-    model: "text-davinci-003",
-    max_tokens: 512,
-    temperature: 0,
-    prompt: prompt,
-  });
-  res.send(completion.data.choices[0].text);
+    const completion = await openai.createCompletion({
+      model: "text-davinci-003",
+      max_tokens: 512,
+      temperature: 0,
+      prompt: prompt,
+    });
+
+    const response = completion.data.choices[0].text;
+    res.status(200).json({ response });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "An error occurred" });
+  }
 });
 
 const PORT = 8080;
@@ -37,5 +37,3 @@ const PORT = 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
 });
-
-//run node server.js
